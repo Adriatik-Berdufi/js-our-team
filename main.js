ec6840f845f58429c1d132e80ee56304bfdaa036
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-23 15:50:19 +0100

    fix: ciclo for da "in " a ""of

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const cardContainer = document.getElementById("cardContainer");
-
+/*
 for( let person in team){
     card = `
         <div class="card box">
@@ -14,5 +14,21 @@ for( let person in team){
     console.log(team[person].name);
     console.log(team[person].role);
     console.log(team[person].image);
+    console.log("------------------")
+}*/
+for( let persona of team){
+    card = `
+    <div class="card box">
+        <div><img src="./img/${persona.image}" alt=""></div>
+        <div class="p-3">
+          <div class="fw-bold">${persona.name}</div>
+          <div>${persona.role}</div>
+        </div>
+    </div>
+`
+    cardContainer.innerHTML += card;
+    console.log(persona.name);
+    console.log(persona.role);
+    console.log(persona.image);
     console.log("------------------")
 }
