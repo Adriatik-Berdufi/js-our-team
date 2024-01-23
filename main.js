5fc3c0e1a926504ee74e628e489748ef489f773d
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-23 14:39:08 +0100

    add:stampa in coslo le info for person

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+for( let person in team){
+    console.log(team[person].name);
+    console.log(team[person].role);
+    console.log(team[person].image);
+    console.log("------------------")
+}
