a4837a6e93ef3b2ab62edcae3996a7d82b09d388
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-23 14:46:09 +0100

    add: add el in dom tramite js

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-
+const cardContainer = document.getElementById("cardContainer");
 
 for( let person in team){
+    card = `
+        <div class="card box">
+            <div><img src="./img/${team[person].image}" alt=""></div>
+            <div class="p-3">
+              <div class="fw-bold">${team[person].name}</div>
+              <div>${team[person].role}</div>
+            </div>
+        </div>
+    `
+    cardContainer.innerHTML += card;
     console.log(team[person].name);
     console.log(team[person].role);
     console.log(team[person].image);
